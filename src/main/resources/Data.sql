9210c68aa802fe325aa8e30ba02ab26463ee5eff
Bandanarama <kvoon0@frostburg.edu>
2021-05-11 18:40:32 -0600

    removed lombok dependency, initialized database for testing

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles(name) VALUES('ROLE_USER');
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+
+INSERT INTO USERS(username, email, password)
+VALUES ('test1', 'test@gmail.com', 'test123');
